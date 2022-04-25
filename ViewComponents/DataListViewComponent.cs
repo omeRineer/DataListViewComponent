35b8bae313e965fb685f2a3f0db49d0b07ee128f
Ömer Faruk Sandıkçı <sandikci_omer@hotmail.com>
2022-04-26 02:01:50 +0300

    DataListViewComponent

---
+++ ViewComponents/DataListViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MVCUI.ViewComponents
+{
+    public class DataListViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke(IEnumerable dataList)
+        {
+            return View(dataList);
+        }
+    }
+}
